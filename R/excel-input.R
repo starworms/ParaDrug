15eb5a1e7ae78dde200cb8de949cf3e8307848d2
Jan Wijffels <jwijffels@bnosac.be>
2025-01-18 14:35:09 +0100

    add fields in output of read_paradrug_xls

--- R/excel-input.R
+++ R/excel-input.R
@@ -9,10 +9,11 @@
 #' \itemize{
 #' \item{data: data.frame with at least columns "Site", "SubjectID", "SchoolID", "Age", "Sex2", "BL_KK2_AL_EPG", "FU_KK2_AL_EPG", "BL_KK2_TT_EPG", "FU_KK2_TT_EPG", "BL_KK2_HW_EPG", "FU_KK2_HW_EPG"}
 #' \item{n: the number of rows in the dataset} 
+#' \item{fields: a character vector of column names in \code{data}}
 #' }
 #' @examples 
 #' path <- system.file(package = "ParaDrug", "extdata", "data", "mydata.xlsx")
-#' x <- read_paradrug_xls(path)
+#' x    <- read_paradrug_xls(path)
 read_paradrug_xls <- function(x, sheet = 1){
     stopifnot(file.exists(x))
     if(is.character(sheet)){
@@ -23,7 +24,7 @@ read_paradrug_xls <- function(x, sheet = 1){
     data <- unclass(data)
     class(data) <- "data.frame"
     n <- nrow(data)
-    out <- list(data = data, n = n)
+    out <- list(data = data, n = n, fields = as.character(unique(na.exclude(colnames(data)))))
     class(out) <- "paradrug_rawdata"
     out
 }
