d84b734342fb4867787b348cdabddbe8f30739b4
Jan Wijffels <jwijffels@bnosac.be>
2025-01-20 23:06:21 +0100

    Upgrade R pkgs

---
+++ inst/apps/paradrug-1.1/renv-init.R
@@ -0,0 +1,10 @@
+library(renv)
+renv::settings$snapshot.type("explicit")
+options(renv.snapshot.ignore.self = FALSE)
+#renv::init(project = "inst/apps/paradrug-1.1")
+renv::init()
+## TO update package to the newest versions
+if(FALSE){
+    renv::update()
+    renv::init()    
+}
--- tools/deploy/renv-init.R
+++
@@ -1,4 +0,0 @@
-library(renv)
-renv::settings$snapshot.type("explicit")
-options(renv.snapshot.ignore.self = FALSE)
-renv::init(project = "inst/apps/paradrug-1.1")
