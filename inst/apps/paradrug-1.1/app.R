f66224ab3b2c8605dbb9fc5d2fda8a87a639df01
Jan Wijffels <jwijffels@bnosac.be>
2025-01-22 21:17:02 +0100

    print installed packages in the app

--- inst/apps/paradrug-1.1/app.R
+++ inst/apps/paradrug-1.1/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(ParaDrug)
+x = installed.packages()[, c("Package", "LibPath", "Version")]
+cat(paste(sprintf("%s: %s --- %s", x[, "Package"], x[, "Version"], x[, "LibPath"]), collapse = "\n"))
 shinyApp(
     ui = paradrugUI(id = "pd"),
     server = paradrugServer)
